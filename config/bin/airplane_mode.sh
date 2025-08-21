9283a134770c7b9180b2f8dd99862d92eadcb0a5
cutelven0k <cutelven0k@icloud.com>
2025-08-21 19:09:04 +0300

    chore: add --help option and improve status toggle functionality

--- config/bin/airplane_mode.sh
+++ config/bin/airplane_mode.sh
@@ -7,6 +7,7 @@ NC="\033[0m"
 
 NOTIF="$HOME/.config/images/airplane.png"
 VERBOSE=0
+HELP=0
 ARGS=()
 
 for arg in "$@"; do
@@ -14,49 +15,82 @@ for arg in "$@"; do
         --verbose)
             VERBOSE=1
             ;;
+        --help)
+            HELP=1
+            ;;
         *)
             ARGS+=("$arg")
             ;;
     esac
 done
 
-if (( VERBOSE )); then
-    check_package.sh --verbose rfkill libnotify || exit 1
-else
-    check_package.sh rfkill libnotify || exit 1
-fi
+print_help() {
+    echo -e "${GREEN}Usage:${NC} $(basename $0) [OPTIONS] [toggle|status]"
+    echo -e "Toggle airplane mode or print the current status."
+    echo -e ""
+    echo -e "${GREEN}Options:${NC}"
+    echo -e "   --verbose     Enable verbose mode to print debug messages."
+    echo -e "   --help        Show this help message."
+    echo -e ""
+    echo -e "${GREEN}Commands:${NC}"
+    echo -e "   toggle        Toggle the airplane mode (block/unblock all devices)."
+    echo -e "                 This will enable or disable the airplane mode by"
+    echo -e "                 blocking or unblocking all devices (WiFi, Bluetooth, etc.)."
+    echo -e "   status        Print the current status of the airplane mode."
+    echo -e "                 It shows whether the airplane mode is ON (all devices blocked)"
+    echo -e "                 or OFF (all devices unblocked)."
+    echo -e ""
+    echo -e "${GREEN}Example:${NC}"
+    echo -e "   $(basename $0) --verbose toggle"
+}
+
+check_dependencies() {
+    if (( VERBOSE )); then
+        check_package.sh --verbose rfkill libnotify || exit 1
+    else
+        check_package.sh rfkill libnotify || exit 1
+    fi
+}
 
 toggle() {
-    local wifi_blocked
-    wifi_blocked=$(rfkill list wifi | grep -o "Soft blocked: yes")
+    local devices_blocked
+    devices_blocked=$(rfkill list | grep -o "Soft blocked: yes")
 
-    if [ -n "$wifi_blocked" ]; then
-        (( VERBOSE )) && echo -e "${YELLOW}WiFi is currently blocked. Unblocking all...${NC}"
+    if [ -n "$devices_blocked" ]; then
+        (( VERBOSE )) && echo -e "${YELLOW}Some devices are currently blocked. Unblocking all...${NC}"
         rfkill unblock all
         notify-send -i "$NOTIF" "Airplane" "mode: OFF"
-        (( VERBOSE )) && echo -e "${GREEN}Airplane mode disabled (WiFi unblocked).${NC}"
+        (( VERBOSE )) && echo -e "${GREEN}Airplane mode disabled (all devices unblocked).${NC}"
     else
-        (( VERBOSE )) && echo -e "${YELLOW}WiFi is currently unblocked. Blocking all...${NC}"
+        (( VERBOSE )) && echo -e "${YELLOW}All devices are currently unblocked. Blocking all...${NC}"
         rfkill block all
         notify-send -i "$NOTIF" "Airplane" "mode: ON"
-        (( VERBOSE )) && echo -e "${GREEN}Airplane mode enabled (WiFi blocked).${NC}"
+        (( VERBOSE )) && echo -e "${GREEN}Airplane mode enabled (all devices blocked).${NC}"
     fi
 }
 
 print_status() {
-    local wifi_blocked
-    wifi_blocked=$(rfkill list wifi | grep -o "Soft blocked: yes")
+    local devices_blocked
+    devices_blocked=$(rfkill list | grep -o "Soft blocked: yes")
 
-    if [ -n "$wifi_blocked" ]; then
-        (( VERBOSE )) && echo -e "${RED}Airplane mode is ON (WiFi blocked).${NC}"
+    if [ -n "$devices_blocked" ]; then
+        (( VERBOSE )) && echo -e "${RED}Airplane mode is ON (all devices blocked).${NC}"
         echo '{"alt": "off"}'
     else
-        (( VERBOSE )) && echo -e "${GREEN}Airplane mode is OFF (WiFi unblocked).${NC}"
+        (( VERBOSE )) && echo -e "${GREEN}Airplane mode is OFF (all devices unblocked).${NC}"
         echo '{"alt": "on"}'
     fi
 }
 
+if (( HELP )) || [ ${#ARGS[@]} -eq 0 ]; then
+    print_help
+    exit 0
+fi
+
+check_dependencies
+
 case "${ARGS[0]}" in
     toggle) toggle ;;
-    *)      print_status ;;
+    status) print_status ;;
+    *)      print_help ;;
 esac
